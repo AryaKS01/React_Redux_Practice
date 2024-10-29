009efdfb905aa5f6fe501aa9fbfde8a37de88c8c
AryaKS01 <aryaworks24@gmail.com>
2024-10-29 22:09:57 +0530

    29th

--- 17 Bootstrap/learning-bootstrap/src/components/Item.jsx
+++ 17 Bootstrap/learning-bootstrap/src/components/Item.jsx
@@ -2,8 +2,21 @@ import styles from "./Item.module.css";
 
 const Item = ({ foodItem }) => {
   // let { foodItem } = props;
+  const handleBuyButtonClicked = (event) => {
+    console.log(event);
+    console.log(`${foodItem} being bought`);
+  };
+
   return (
-    <li className={` ${styles["kg-item"]} list-group-item`}>{foodItem}</li>
+    <li className={` ${styles["kg-item"]} list-group-item`}>
+      {foodItem}
+      <button
+        className={`${styles.button} btn btn-info`}
+        onClick={(event) => handleBuyButtonClicked(event)}
+      >
+        Buy
+      </button>
+    </li>
   );
 };
 
