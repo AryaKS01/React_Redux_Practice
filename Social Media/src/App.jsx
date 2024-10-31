2ce0374ee05057e272de126807c994b2d287ec8c
AryaKS01 <aryaworks24@gmail.com>
2024-10-31 22:23:10 +0530

    Social Media Project

---
+++ Social Media/src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import Post from "./components/post";
+
+function App() {
+  return (
+    <>
+      <div className="app-container">
+        <Sidebar></Sidebar>
+        <div className="content">
+          <Header></Header>
+          <CreatePost></CreatePost>
+          <Post></Post>
+          <Footer></Footer>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
